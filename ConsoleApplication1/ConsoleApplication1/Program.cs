e77282669056f34183e86c700d9e61e83f785407
skraghava <skraghava@gmail.com>
2017-01-30 11:58:06 +0530

    New dummy project added

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodByMasterBranch();
+            Console.ReadKey();
+        }
+        static void MethodByMasterBranch()
+        {
+            Console.WriteLine("Method added by master branch");
+        }
+    }
+}
